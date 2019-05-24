3b1ee706b73b14217e7f9d570c957dd5d9040e68
BNPrashanth <bnprashanth256@gmail.com>
2019-05-24 14:05:37 +0530

    uber zap logger initialization implemented

--- configs/init.go
+++ configs/init.go
@@ -10,6 +10,13 @@ import (
 	"fmt"
 
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	// Log variable is a globally accessible variable which will be initialized when the InitializeZapCustomLogger function is executed successfully.
+	Log *zap.Logger
 )
 
 /*
@@ -30,5 +37,20 @@ func InitializeViper() {
 InitializeZapCustomLogger Funtion initializes a logger using uber-go/zap package in the application.
 */
 func InitializeZapCustomLogger() {
-
+	conf := zap.Config{
+		Encoding:    "json",
+		Level:       zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		OutputPaths: []string{viper.GetString("logger-output-path"), "stdout"},
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			CallerKey:    "file",
+			MessageKey:   "msg",
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	Log, _ = conf.Build()
+	Log.Info("Logger Initialized..")
 }
