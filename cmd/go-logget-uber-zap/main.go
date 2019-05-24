4e8d7a33221d886118cbb81cb56743d742e647ea
BNPrashanth <bnprashanth256@gmail.com>
2019-05-24 12:20:24 +0530

    config.example updated, viper initialization tested

--- cmd/go-logget-uber-zap/main.go
+++ cmd/go-logget-uber-zap/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	c "github.com/BNPrashanth/go-logger-uber-zap/configs"
+
+	"github.com/spf13/viper"
+)
+
 func main() {
-	
+	c.InitializeViper()
+	fmt.Println(viper.GetString("logger-output-path"))
 }
