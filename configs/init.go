2753da20d6cdb8bf97ffc7408c8c5fa6be9dc8e9
BNPrashanth <bnprashanth256@gmail.com>
2019-05-24 12:15:29 +0530

    viper initialization function created

--- configs/init.go
+++ configs/init.go
@@ -1,13 +1,34 @@
+/*
+Package configs implements configuration functio needed to run the application.
+It contains the initialization functions to enable,
+	Viper
+	Uber-Zap-Custom-Logger
+*/
 package configs
 
-func init() {
+import (
+	"fmt"
 
+	"github.com/spf13/viper"
+)
+
+/*
+InitializeViper Funtion initializes viper to read configuration and/or environment variables in the application.
+*/
+func InitializeViper() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
 }
 
-func initializeViper() {
-
-}
-
-func initializeZapCustomLogger() {
+/*
+InitializeZapCustomLogger Funtion initializes a logger using uber-go/zap package in the application.
+*/
+func InitializeZapCustomLogger() {
 
 }
