57b3d2f3f6ebc24f7163ee449753b5e737ca922e
BNPrashanth <bnprashanth256@gmail.com>
2019-05-27 09:28:57 +0530

    multiple oggers created

--- configs/init.go
+++ configs/init.go
@@ -15,8 +15,11 @@ import (
 )
 
 var (
-	// Log variable is a globally accessible variable which will be initialized when the InitializeZapCustomLogger function is executed successfully.
-	Log *zap.Logger
+	// GenLog variable is a globally accessible variable which will be initialized when the InitializeZapCustomLogger function is executed successfully.
+	GenLog *zap.Logger
+
+	// ErrLog variable is a globally accessible variable which will be initialized when the InitializeZapCustomLogger function is executed successfully.
+	ErrLog *zap.Logger
 )
 
 /*
@@ -37,20 +40,49 @@ func InitializeViper() {
 InitializeZapCustomLogger Funtion initializes a logger using uber-go/zap package in the application.
 */
 func InitializeZapCustomLogger() {
+	GenLog = initializeGeneralLogger()
+	ErrLog = initializeErrorLogger()
+	GenLog.Info("Logger Initialized..")
+}
+
+func initializeGeneralLogger() *zap.Logger {
 	conf := zap.Config{
 		Encoding:    "json",
 		Level:       zap.NewAtomicLevelAt(zapcore.InfoLevel),
 		OutputPaths: []string{viper.GetString("logger-output-path"), "stdout"},
 		EncoderConfig: zapcore.EncoderConfig{
+			NameKey:      "log",
 			LevelKey:     "level",
 			TimeKey:      "time",
 			CallerKey:    "file",
 			MessageKey:   "msg",
+			EncodeName:   zapcore.FullNameEncoder,
 			EncodeLevel:  zapcore.LowercaseLevelEncoder,
 			EncodeTime:   zapcore.ISO8601TimeEncoder,
 			EncodeCaller: zapcore.ShortCallerEncoder,
 		},
 	}
-	Log, _ = conf.Build()
-	Log.Info("Logger Initialized..")
+	Log, _ := conf.Build()
+	return Log
+}
+
+func initializeErrorLogger() *zap.Logger {
+	conf := zap.Config{
+		Encoding:    "json",
+		Level:       zap.NewAtomicLevelAt(zapcore.ErrorLevel),
+		OutputPaths: []string{viper.GetString("logger-output-path"), "stdout"},
+		EncoderConfig: zapcore.EncoderConfig{
+			NameKey:      "log",
+			LevelKey:     "level",
+			TimeKey:      "time",
+			CallerKey:    "file",
+			MessageKey:   "msg",
+			EncodeName:   zapcore.FullNameEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	Log, _ := conf.Build()
+	return Log
 }
